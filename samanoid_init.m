789334fa165e2e696bea183a87013d1ddacff710
samvrit <samvrit.goku@gmail.com>
2023-09-17 19:03:35 -0700

    Inverse kinematics uses oritentation and do side weight shift

--- samanoid_init.m
+++ samanoid_init.m
@@ -12,7 +12,8 @@ theta_guess = deg2rad([0; 20; -40; 20; 0]);
 w = 2*pi*0.5;
 t = 0:0.001:9.999;
 
-COM_traj = COM_init(3) + 0.05 * sin(w*t);
+COM_traj_right = COM_init(2) + 0.1 * sin(w*t);
+COM_traj_left = -COM_init(2) + 0.1 * sin(w*t);
 
 joint_traj_right = theta_init .* ones(5, size(t, 2));
 joint_traj_left = theta_init .* ones(5, size(t, 2));
@@ -23,9 +24,9 @@ num_iterations_left = zeros(1, size(t, 2));
 joint_traj_right(:,1) = theta_init;
 joint_traj_left(:,1) = theta_init;
 
-for i = 2:size(COM_traj, 2)
-    [joint_traj_right(:,i), num_iterations_right(i)] = inv_kinematics(COM_init(1), COM_init(2), COM_traj(i), joint_traj_right(1,i-1), joint_traj_right(2,i-1), joint_traj_right(3,i-1), joint_traj_right(4,i-1), joint_traj_right(5,i-1), 'right');
-    [joint_traj_left(:,i), num_iterations_left(i)] = inv_kinematics(COM_init(1), -COM_init(2), COM_traj(i), joint_traj_left(1,i-1), joint_traj_left(2,i-1), joint_traj_left(3,i-1), joint_traj_left(4,i-1), joint_traj_left(5,i-1), 'left');
+for i = 2:size(COM_traj_right, 2)
+    [joint_traj_right(:,i), num_iterations_right(i)] = inv_kinematics(COM_init(1), COM_traj_right(i), COM_init(3), joint_traj_right(1,i-1), joint_traj_right(2,i-1), joint_traj_right(3,i-1), joint_traj_right(4,i-1), joint_traj_right(5,i-1), 'right');
+    [joint_traj_left(:,i), num_iterations_left(i)] = inv_kinematics(COM_init(1), COM_traj_left(i), COM_init(3), joint_traj_left(1,i-1), joint_traj_left(2,i-1), joint_traj_left(3,i-1), joint_traj_left(4,i-1), joint_traj_left(5,i-1), 'left');
 end
 
 theta_right_timeseries = timeseries(joint_traj_right, t);
